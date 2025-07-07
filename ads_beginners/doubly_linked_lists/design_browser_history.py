9504a4e04d4bb44217dca78ace78c2e63bec9fef
tcapbuttercup <tylerbuttercake@gmail.com>
2025-07-06 21:19:06 -0400

    add type hinting import

---
+++ ads_beginners/doubly_linked_lists/design_browser_history.py
@@ -0,0 +1,28 @@
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.history = [homepage]  # Store all visited URLs
+        self.curr = 0  # Pointer to current page
+        self.last = 0  # Farthest forward you can go (clears if you visit)
+
+    def visit(self, url: str) -> None:
+        self.curr += 1
+        if self.curr == len(self.history):
+            self.history.append(url)
+        else:
+            self.history[self.curr] = url
+        self.last = self.curr  # visiting a new URL clears forward history
+
+    def back(self, steps: int) -> str:
+        self.curr = max(0, self.curr - steps)
+        return self.history[self.curr]
+
+    def forward(self, steps: int) -> str:
+        self.curr = min(self.last, self.curr + steps)
+        return self.history[self.curr]
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
